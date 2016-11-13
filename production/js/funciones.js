85fed8bea5fd0f30a018caec9eee01cfbb1528a5
guilhermebussidias <guilherme17.dias@gmail.com>
2016-11-13 13:12:30 +0100

    funciones.js

---
+++ production/js/funciones.js
@@ -0,0 +1,48 @@
+function addRow(){
+  $('#tabla-hijos tr:last').after('<tr>' +
+    '<td>' +
+        '<select class="form-control">' +
+          '<option>2012</option>' +
+          '<option>2011</option>' +
+          '<option>2010</option>' +
+          '<option>2009</option>' +
+          '<option>2008</option>' +
+          '<option>2007</option>' +
+          '<option>2006</option>' +
+          '<option>2005</option>' +
+        '</select>' +
+    '</td>' +
+    '<td>' +
+      '<select class="form-control">' +
+        '<option>2012</option>' +
+        '<option>2011</option>' +
+        '<option>2010</option>' +
+        '<option>2009</option>' +
+        '<option>2008</option>' +
+        '<option>2007</option>' +
+        '<option>2006</option>' +
+        '<option>2005</option>' +
+      '</select>' +
+    '</td>' +
+    '<td>' +
+        '<input type="radio" name="discapacidad" value="33-65">' +
+  '  </td>' +
+    '<td>' +
+      '<input type="radio" name="discapacidad" value="65">' +
+    '</td>' +
+    '<td>' +
+      '<input type="checkbox" name="discapacidad" value="ayuda">' +
+    '</td>' +
+    '<td>' +
+      '<input type="checkbox" name="discapacidad" value="convivencia">' +
+    '</td>' +
+    '<td>' +
+      '<button type = "button" class="btn btn-danger">Borrar</button>' +
+    '</td>' +
+  '</tr>');
+
+}
+
+function removeRow(){
+  $('#tabla-hijos').children('tr').remove();
+}
