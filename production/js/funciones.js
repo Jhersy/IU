78840adcb46b7ee2cc1359c0ae230e3abdfa291a
guilhermebussidias <guilherme17.dias@gmail.com>
2016-11-14 19:26:54 +0100

    Añadido popover en tablas pantalla 2

--- production/js/funciones.js
+++ production/js/funciones.js
@@ -1,12 +1,10 @@
-function addRow(tabla){
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover();
+});
 
-  if(tabla == "tabla-hijos"){
-    var borrar = "removeRow";
-  }else{
-    var borrar = "removeRowAscendientes";
-  }
+function addRow(){
 
-  $('#' + tabla + ' tr:last').after('<tr>' +
+  $('#tabla-hijos tr:last').after('<tr>' +
     '<td>' +
         '<select class="form-control">' +
           '<option>2012</option>' +
@@ -44,10 +42,43 @@ function addRow(tabla){
       '<input type="radio" name="convivencia" value="convivencia">' +
     '</td>' +
     '<td>' +
-      '<button type = "button" class="btn btn-danger" onclick="'+ borrar +'(this)">Borrar</button>' +
+      '<button type = "button" class="btn btn-danger" onclick="removeRow(this)">Borrar</button>' +
     '</td>' +
   '</tr>');
 
+}
+
+function addRowAscendientes(){
+
+  $('#tabla-ascendientes tr:last').after('<tr>' +
+    '<td>' +
+        '<select class="form-control">' +
+          '<option>2012</option>' +
+          '<option>2011</option>' +
+          '<option>2010</option>' +
+          '<option>2009</option>' +
+          '<option>2008</option>' +
+          '<option>2007</option>' +
+          '<option>2006</option>' +
+          '<option>2005</option>' +
+        '</select>' +
+    '</td>' +
+    '<td>' +
+        '<input type="radio" name="discapacidad" value="33-65">' +
+  '  </td>' +
+    '<td>' +
+      '<input type="radio" name="discapacidad" value="65">' +
+    '</td>' +
+    '<td>' +
+      '<input type="radio" name="ayuda" value="ayuda">' +
+    '</td>' +
+    '<td>' +
+      '<input type="text" class="col-md-2 col-sm-2 col-xs-2">' +
+    '</td>' +
+    '<td>' +
+      '<button type = "button" class="btn btn-danger" onclick="removeRowAscendientes(this)">Borrar</button>' +
+    '</td>' +
+  '</tr>');
 }
 
 function removeRow(fila){
